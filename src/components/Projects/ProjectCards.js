0d596a6b19712e181fc203a299c87eb9dff0a3cb
Akash <akashrajmane23@gmail.com>
2024-05-29 00:16:15 +0530

    card styles added

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState} from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  const [highlightStyle, setHighlightStyle] = useState({
+    top: "50%",
+    left: "50%",
+  });
+
+  const handleMouseMove = (e) => {
+    const { left, top } = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+    setHighlightStyle({ top: `${y}px`, left: `${x}px` });
+  };
+
   return (
-    <Card className="project-card-view">
+    <Card className="project-card-view" onMouseMove={handleMouseMove}>
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
+        <div className="highlight" style={highlightStyle}></div>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           <span>Stack:</span><br/>
